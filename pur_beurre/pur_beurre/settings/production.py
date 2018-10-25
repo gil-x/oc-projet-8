6accd9e08f03080ad62ea33557d96782936cfc0b
gil-x <gino.ladowitch@gmail.com>
2018-10-25 12:12:36 +0200

    added Procfile and production settings, truly? the last?

--- pur_beurre/pur_beurre/settings/production.py
+++ pur_beurre/pur_beurre/settings/production.py
@@ -11,10 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-<<<<<<< HEAD
 import dj_database_url
-=======
->>>>>>> faa06a247a9d1b3ec3c755890a38b04e38aee23f
 
 def get_env_variable(var_name):
     """Get the environment variable or return exception."""
