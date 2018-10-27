02b0205e8978a25c2f754dd2ab0f7fa7118da114
gil-x <gino.ladowitch@gmail.com>
2018-10-27 15:31:59 +0200

    OUPS! forgot to put false, no matter, a new commitgit push origin mastergit push origin master

--- pur_beurre/settings/local.py
+++ pur_beurre/settings/local.py
@@ -28,7 +28,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'purb.herokuapp.com']
 
