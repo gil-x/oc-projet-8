5e50f4c571aa5a71c45e8564e69c312b9b9786ed
gil-x <gino.ladowitch@gmail.com>
2018-10-27 15:24:39 +0200

    add whitenoise middleware

--- pur_beurre/settings/heroku.py
+++ pur_beurre/settings/heroku.py
@@ -16,7 +16,16 @@ import django_heroku
 
 DEBUG = False
 
-
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
 
 
 django_heroku.settings(locals())
